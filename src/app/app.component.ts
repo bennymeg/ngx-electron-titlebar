c3fb73daebc5a273e5f42642095a3c734d5cc748
Benny Megidish <bennymegk@gmail.com>
2021-08-15 02:08:16 +0300

    chore: updated readme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,12 @@ import { faGithub, faNpm } from '@fortawesome/free-brands-svg-icons';
 })
 export class AppComponent {
   theme: 'light' | 'dark' = 'dark';  // theme style
-  draggable : boolean = true;        // enables/disables the -webkit-app-region CSS property on the root element. Allows frameless windows to be dragged in an electron application
+  draggable: boolean = true;         // enables/disables the -webkit-app-region CSS property on the root element. Allows frameless windows to be dragged in an electron application
   fixed: boolean = false;            // affixes to the top and floats above the rest of the content so only the buttons and title are visible. Ignores transparent rule
   os: 'mac' | 'win' | 'default' = 'mac';  // titlebar style (defaults to current OS, or default if unrecognized)
   tall: boolean = false;             // makes the titlebar taller than usual, with the controls slightly inset (mac only)
   title: string = 'Title';           // app title
-  transparent : boolean = false;     // transparent background for an overlay effect
+  transparent: boolean = false;      // transparent background for an overlay effect
   rtl: boolean = false;              // right to left alignment
 
   faGithub = faGithub;
